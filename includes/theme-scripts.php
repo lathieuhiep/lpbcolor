1b3f40ea48866120c64f75f7bf17402a987a81fc
lathieuhiep <khacdiepkma90@gmail.com>
2025-03-30 22:17:34 +0700

    update code

--- includes/theme-scripts.php
+++ includes/theme-scripts.php
@@ -46,14 +46,6 @@ add_action( 'wp_head', function() {
 }, 5);
 
 function lpbcolor_register_front_end(): void {
-	// remove style gutenberg
-	wp_dequeue_style('wp-block-library');
-	wp_dequeue_style('wp-block-library-theme');
-	wp_dequeue_style( 'classic-theme-styles' );
-
-	wp_dequeue_style('wc-blocks-style');
-	wp_dequeue_style('storefront-gutenberg-blocks');
-
 	/** Load css **/
 
 	// bootstrap css
